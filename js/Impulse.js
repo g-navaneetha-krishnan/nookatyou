4817fb79e95e570dfd4651754e0c61d9cc5ccadb
g-navaneetha-krishnan <connectwithnavaneeth@gmail.com>
2025-05-20 18:14:05 +0530

    first commit

---
+++ js/Impulse.js
@@ -0,0 +1,3 @@
+window.wishlisthero_cartDotClasses = (window.wishlisthero_cartDotClasses || []).concat(['cart-link__bubble', 'cart-link__bubble--visible', 'is-visible']);
+window.wishlisthero_buttonProdPageClasses = (window.wishlisthero_buttonProdPageClasses || []).concat(['btn', 'btn--full', 'btn--secondary']);
+window.wishlisthero_buttonProdPageFontSize = 'auto';
